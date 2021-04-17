f231a56f12460569ec3e9418870cb3a6a2438fb2
Kátia Caroline <katiacaroline011@gmail.com>
2021-04-17 13:20:31 -0300

    printando nomes

--- src/main/java/br/edu/facthus/apoo/Main.java
+++ src/main/java/br/edu/facthus/apoo/Main.java
@@ -2,9 +2,10 @@ package br.edu.facthus.apoo;
 
 public class Main {
 
-	public static void exercicioGrupo0() {
-		System.out.println("Fulano de Tal");
-		System.out.println("Cicrano de Tal");
+	public static void exercicioGrupo5() {
+		System.out.println("Breno Oliveira");
+		System.out.println("Kátia Alves");
+		System.out.println("Marcos Paulo");
 	}
 
 	// TODO: acrescentar os outros grupos
@@ -12,7 +13,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Projeto APOO - 2021/01");
 
-		exercicioGrupo0();
+		exercicioGrupo5();
 	}
 
 }
