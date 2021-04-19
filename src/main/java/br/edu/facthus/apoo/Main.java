12f8f2eff6c5503d6df18236fa93cc18f904c5dd
Fernando Mattioli <mattioli.fernando@gmail.com>
2021-04-19 20:42:23 -0300

    Corrige o arquivo Main.java.

--- src/main/java/br/edu/facthus/apoo/Main.java
+++ src/main/java/br/edu/facthus/apoo/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
 	public static void exercicioGrupo08() {
 		System.out.println("OLiver Andrade dos Santos");
-  }
+	}
 
-  public static void exercicioGrupo5() {
+	public static void exercicioGrupo5() {
 		System.out.println("Breno Oliveira");
 		System.out.println("Kátia Alves");
 		System.out.println("Marcos Paulo");
@@ -17,16 +17,15 @@ public class Main {
 		System.out.println("Pedro Paulo Nascimento Pessoa");
 		System.out.println("Claudionor Freitas de Sousa");
 		System.out.println("Bruna Moura Costa");
-  }
-
+	}
 
 	public static void exercicioGrupo04() {
 		System.out.println("IURI DE OLIVEIRA");
 		System.out.println("ALISON ADRIAO CERVO");
-		System.out.println("MIRYA VIT�RIA RIOS JULIANI");
+		System.out.println("MIRYA VITÓRIA RIOS JULIANI");
 		System.out.println("RANIELLE DA SILVA SANTOS");
 	}
-	
+
 	public static void exercicioGrupo2() {
 		System.out.println("Grupo 2:");
 		System.out.println("João Ian Cruz Cardoso");
@@ -36,23 +35,21 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-    System.out.println("Projeto APOO - 2021/01");
+		System.out.println("Projeto APOO - 2021/01");
 
- 		exercicioGrupo0();
-		exercicioGrupo1();
 		exercicioGrupo2();
 
-    System.out.println("Trabalho de Análise e projeto orientado ao objeto");
+		System.out.println("Trabalho de Análise e projeto orientado ao objeto");
 		System.out.println("-------< Grupo 03 >-------\n");
 		grupoTrabalhoApooGrupo03();
-    
-    System.out.println("Projeto POO - 12/04/2021");
+
+		System.out.println("Projeto POO - 12/04/2021");
 		System.out.println(">> GRUPO 04 <<");
 		exercicioGrupo04();
-    
- 		exercicioGrupo5();
-    
- 		exercicioGrupo08();
+
+		exercicioGrupo5();
+
+		exercicioGrupo08();
 
 	}
 }
