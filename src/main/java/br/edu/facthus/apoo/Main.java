3fc6648da1fe5613c6f9a1f14e6e129380e29593
Joao <joaoiancruz@gamil.com>
2021-04-19 20:34:02 -0300

    Comit Grupo 02

--- src/main/java/br/edu/facthus/apoo/Main.java
+++ src/main/java/br/edu/facthus/apoo/Main.java
@@ -2,26 +2,21 @@ package br.edu.facthus.apoo;
 
 public class Main {
 
-	public static void exercicioGrupo0() {
-		System.out.println("Fulano de Tal");
-		System.out.println("Cicrano de Tal");
-	}
 
-	// TODO: acrescentar os outros grupos
 	
-	public static void exercicioGrupo1() {
-		System.out.println("Grupo 1:");
-		System.out.println("Cleiton");
-		System.out.println("Giulia Victoria Oliveira Crozara");
-		System.out.println("João Pedro Oliveira da Silva");
-		System.out.println("Mário Eduardo Gonçalves Fortes Filho");
+	public static void exercicioGrupo2() {
+		System.out.println("Grupo 2:");
+		System.out.println("João Ian Cruz Cardoso");
+		System.out.println("Douglas Nunes");
+		System.out.println("Wellignton Cardosos Ingles");
+		System.out.println("Guilherme");
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Projeto APOO - 2021/01");
 
-		exercicioGrupo0();
-		exercicioGrupo1();
+		
+		exercicioGrupo2();
 	}
 
 }
