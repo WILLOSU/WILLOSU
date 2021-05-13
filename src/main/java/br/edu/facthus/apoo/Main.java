382da4c6e32640dec889d8e7d5314e4502d59b08
ThyagoMariano <74057836+ThyagoMariano@users.noreply.github.com>
2021-05-12 22:59:53 -0300

    acrescenta nome

--- src/main/java/br/edu/facthus/apoo/Main.java
+++ src/main/java/br/edu/facthus/apoo/Main.java
@@ -1,78 +1,12 @@
 package br.edu.facthus.apoo;
 
-public class Main {
+public class Main 
+{
 
-	public static void exercicioGrupo08() {
-		System.out.println("OLiver Andrade dos Santos");
+	public static void exercicioGrupo08() 
+	{
+		System.out.println("THYAGO MARIANO DE OLIVEIRA PONTES");
 	}
 
-	public static void exercicioGrupo5() {
-		System.out.println("Breno Oliveira");
-		System.out.println("Kátia Alves");
-		System.out.println("Marcos Paulo");
-	}
 	
-	
-	public static void exercicioGrupo6() {
-		System.out.println("William de Sousa Mota");
-		System.out.println("Jéssica Cunha da Silva");
-		System.out.println("Breno Silva Amaral");
-	}
-
-	public static void grupoTrabalhoApooGrupo03() {
-		System.out.println("Thiago Nogueira Pires");
-		System.out.println("Pedro Paulo Nascimento Pessoa");
-		System.out.println("Claudionor Freitas de Sousa");
-		System.out.println("Bruna Moura Costa");
-	}
-
-	public static void exercicioGrupo04() {
-		System.out.println("IURI DE OLIVEIRA");
-		System.out.println("ALISON ADRIAO CERVO");
-		System.out.println("MIRYA VITÓRIA RIOS JULIANI");
-		System.out.println("RANIELLE DA SILVA SANTOS");
-	}
-
-	public static void exercicioGrupo2() {
-		System.out.println("Grupo 2:");
-		System.out.println("João Ian Cruz Cardoso");
-		System.out.println("Douglas Nunes");
-		System.out.println("Wellignton Cardosos Ingles");
-		System.out.println("Guilherme");
-	}
-	
-	public static void exercicioGrupo7() {	
-		System.out.println("Pierre Mendes Salatiel");
-		System.out.println("Igor Sampaio Ribeiro");
-		System.out.println("Thyago Mariano de Oliveira Pontes");
-	}
-	
-	public static void exercicioGrupo09() {
-		System.out.println("Luiz Henriique dos Santos");
-	}
-
-	public static void main(String[] args) {
-		System.out.println("Projeto APOO - 2021/01");
-
-		exercicioGrupo2();
-
-		System.out.println("Trabalho de Análise e projeto orientado ao objeto");
-		System.out.println("-------< Grupo 03 >-------\n");
-		grupoTrabalhoApooGrupo03();
-
-		System.out.println("Projeto POO - 12/04/2021");
-		System.out.println(">> GRUPO 04 <<");
-		exercicioGrupo04();
-
-		exercicioGrupo5();
-		
-		exercicioGrupo6();
-		
-		exercicioGrupo7();
-
-		exercicioGrupo08();
-		
-		exercicioGrupo09();
-
-	}
 }
